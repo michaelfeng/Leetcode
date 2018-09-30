79af062d84277a73bc902072b47533840fe3e390
Michael Feng <michaelfeng1990@gmail.com>
2018-09-30 15:18:22 +0800

    solution for problem 231

---
+++ 231_Power_of_Two/main.py
@@ -0,0 +1,8 @@
+import math
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and (n & (n - 1) == 0)
