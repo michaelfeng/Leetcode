64459c02ec6fb7b5b5d8370842c126b8d0d0bd33
Michael Feng <michaelfeng1990@gmail.com>
2018-10-04 16:01:37 +0800

    solution for problem 658 with logn implementation

---
+++ 658_Find_K_Closest_Elements/main_logn.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        if not arr or len(arr) == 0: return []
+        
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid+k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left+k]
