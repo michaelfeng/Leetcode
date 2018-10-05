f17fd531f1343124b51157e0e35aef59df1cec21
Michael Feng <michaelfeng1990@gmail.com>
2018-10-05 14:32:16 +0800

    solution for problem 680

---
+++ 680_Valid_Palindrome_II/main.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i, j = 0, len(s) - 1
+        count = 0
+        while i < j:
+            if s[i] != s[j]:
+                return self.check(s[i:j]) or self.check(s[i+1:j+1])
+            else:
+                i, j = i + 1, j - 1        
+        return  True  
+    
+    def check(self, s):
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i, j = i + 1, j - 1
+            else:
+                return False
+        return True
+        
