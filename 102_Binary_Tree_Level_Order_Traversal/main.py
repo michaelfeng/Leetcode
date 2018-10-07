4a0aa58250429d78f7d364a6c48a891d53996699
Michael Feng <michaelfeng1990@gmail.com>
2018-10-07 19:24:36 +0800

    solution for classical problem with BFS

---
+++ 102_Binary_Tree_Level_Order_Traversal/main.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root: return []
+        
+        queue = deque([root])
+        ans = []
+        while queue:
+            level = []
+            for _ in xrange(len(queue)):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(level)
+        return ans
